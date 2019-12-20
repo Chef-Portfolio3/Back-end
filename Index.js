9fc9bf8bc9469e6e10ea5ceb56a62c63a542e9ed
Carlos Mitchell <kakismitchell@gmail.com>
2019-12-20 01:32:31 -0500

    console.log on index.js

--- Index.js
+++ Index.js
@@ -4,5 +4,5 @@ const server = require('./server.js');
 const port = process.env.PORT;
 
 server.listen(port, () => {
-  console.log(`🍀  🤖   Listening on port ${PORT}...`)
+  console.log(`Listening on port ${PORT}...`)
 });
