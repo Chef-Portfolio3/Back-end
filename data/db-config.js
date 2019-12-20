c6776f97339d05dbb4196ddc27b361d9bb63b6b8
Carlos Mitchell <kakismitchell@gmail.com>
2019-12-19 23:23:21 -0500

    bdconfig fix

--- data/db-config.js
+++ data/db-config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const knex = require('knex');
 
 const config = require("../knexfile.js");
-const environment = process.env.DB_CONNECT || "development";
+const environment = process.env.DB_ENV || "development";
 
 
 module.exports = knex(config[environment]);
