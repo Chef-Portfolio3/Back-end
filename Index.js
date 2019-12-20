45f40f4425d8af58d2d652fc669289781d820ca8
Carlos Mitchell <kakismitchell@gmail.com>
2019-12-19 22:03:56 -0500

    added env

--- Index.js
+++ Index.js
@@ -1,6 +1,7 @@
 const server = require('./server.js');
+require('dotenv').config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
   console.log(`🍀  🤖   Listening on port ${PORT}...`)
